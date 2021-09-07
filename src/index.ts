2a485b88fd83405a07d61121da28e43908d989d0
Raiane Campos <raianejanu@hotmail.com>
2021-09-07 19:48:09 -0300

    feat: complete the challenge 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
-
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerFile));
+
 app.use("/users", usersRoutes);
+
 
 export { app };
